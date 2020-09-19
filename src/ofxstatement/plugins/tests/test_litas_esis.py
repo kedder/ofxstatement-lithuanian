4dd501a85563b7e0b8be68610baf40f1000b266f
Andrey Lebedev <andrey@lebedev.lt>
2020-09-19 14:45:44 +0300

    Restore test for litas-esis plugin

--- src/ofxstatement/plugins/tests/test_litas_esis.py
+++ src/ofxstatement/plugins/tests/test_litas_esis.py
@@ -1,94 +1,57 @@
-import doctest
+import os
+import datetime
+from decimal import Decimal
 
 from ofxstatement.plugins.litas_esis import LitasEsisCsvStatementParser
 
 
-def doctest_LitasEsisCsvStatementParser():
-    """Test LitasEsisCsvStatementParser
+def test_LitasEsisCsvStatementParser() -> None:
 
-    Open sample csv to parse
-        >>> import os
-        >>> csvfile = os.path.join(os.path.dirname(__file__),
-        ...                        'samples', 'litas_esis.acc')
+    # Open sample csv to parse
+    csvfile = os.path.join(os.path.dirname(__file__), "samples", "litas_esis.acc")
 
-    Create parser object and parse:
-        >>> fin = open(csvfile, 'r', encoding='cp1257')
-        >>> parser = LitasEsisCsvStatementParser(fin)
-        >>> statement = parser.parse()
+    # Create parser object and parse:
+    fin = open(csvfile, "r", encoding="cp1257")
+    parser = LitasEsisCsvStatementParser(fin)
+    statement = parser.parse()
 
-    Check what we've got:
-        >>> statement.account_id
-        'LT000000000000000000LTL'
-        >>> len(statement.lines)
-        7
-        >>> statement.start_balance
-        Decimal('251.75')
-        >>> statement.start_date
-        datetime.datetime(2012, 1, 1, 0, 0)
-        >>> statement.end_balance
-        Decimal('74.83')
-        >>> statement.end_date
-        datetime.datetime(2012, 3, 4, 0, 0)
-        >>> statement.currency
-        'LTL'
+    # Check what we've got:
+    assert statement.account_id == "LT000000000000000000LTL"
+    assert len(statement.lines) == 7
+    assert statement.start_balance == Decimal("251.75")
+    assert statement.start_date == datetime.datetime(2012, 1, 1, 0, 0)
+    assert statement.end_balance == Decimal("74.83")
+    assert statement.end_date == datetime.datetime(2012, 3, 4, 0, 0)
+    assert statement.currency == "LTL"
 
-    Check first line:
-        >>> l = statement.lines[0]
-        >>> l.amount
-        Decimal('-1')
-        >>> l.payee
-        'AB DNB BANKAS'
-        >>> l.memo
-        'Mokestis už sąskaitos aptarnavimą'
-        >>> l.id
-        '1987555498'
+    # Check first line:
+    line = statement.lines[0]
+    assert line.amount == Decimal("-1")
+    assert line.payee == "AB DNB BANKAS"
+    assert line.memo == "Mokestis už sąskaitos aptarnavimą"
+    assert line.id == "1987555498"
 
-    Check credit line:
-        >>> l = statement.lines[3]
-        >>> l.amount
-        Decimal('300')
-        >>> l.payee
-        'LINUS TORVALDS'
-        >>> l.memo
-        'Hello World'
-        >>> l.id
-        '2003969289'
-
-    """
+    # Check credit line:
+    line = statement.lines[3]
+    assert line.amount == Decimal("300")
+    assert line.payee == "LINUS TORVALDS"
+    assert line.memo == "Hello World"
+    assert line.id == "2003969289"
 
 
-def doctest_LitasEsisCsvStatementParser_swap_payee_memo():
-    """Test ability to swap payee and memo in LitasEsisCsvStatementParser
+def test_LitasEsisCsvStatementParser_swap_payee_memo() -> None:
+    # Test ability to swap payee and memo in LitasEsisCsvStatementParser
 
-    Open sample csv to parse
-        >>> import os
-        >>> csvfile = os.path.join(os.path.dirname(__file__),
-        ...                        'samples', 'litas_esis.acc')
+    # Open sample csv to parse
+    csvfile = os.path.join(os.path.dirname(__file__), "samples", "litas_esis.acc")
 
-    Create parser object and parse:
-        >>> fin = open(csvfile, 'r', encoding='cp1257')
-        >>> parser = LitasEsisCsvStatementParser(fin)
-        >>> parser.swap_payee_and_memo()
-        >>> statement = parser.parse()
+    # Create parser object and parse:
+    fin = open(csvfile, "r", encoding="cp1257")
+    parser = LitasEsisCsvStatementParser(fin)
+    parser.swap_payee_and_memo()
+    statement = parser.parse()
 
-    Memo and payee should be swapped
-        >>> l = statement.lines[3]
-        >>> l.memo
-        'LINUS TORVALDS'
-        >>> l.payee
-        'Hello World'
-    """
-
-
-def test_suite(*args):
-    return doctest.DocTestSuite(
-        optionflags=(
-            doctest.NORMALIZE_WHITESPACE
-            | doctest.ELLIPSIS
-            | doctest.REPORT_ONLY_FIRST_FAILURE
-            | doctest.REPORT_NDIFF
-        )
-    )
-
-
-load_tests = test_suite
+    # Memo and payee should be swapped
+    line = statement.lines[3]
+    assert line.memo == "LINUS TORVALDS"
+    assert line.payee == "Hello World"
